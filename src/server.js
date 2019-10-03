bde6d6b6858d347b2ebc2908e26044263d12a4c6
Carlos Vinicius Ferrareze <viniferrareze@gmail.com>
2019-10-03 16:37:29 -0300

    adicionado novos recursos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 //recebe o express
 const app = express();
@@ -10,11 +12,17 @@ mongoose.connect('mongodb+srv://vini:vini123@bancoapi-xvgwp.mongodb.net/aricnc?r
    useUnifiedTopology: true
 });
 
+//qualquer aplicação pode acessar a api
+app.use(cors());
+
 //o express utiliza o formato json
 app.use(express.json()); 
+
+//retorna arquivos estaticos
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 //utiliza o arquivo de rotas
 app.use(routes);
 
 //inicia a porta
-app.listen(3000); 
+app.listen(3333); 
